6089014da891b4d1167f8d0d1e703c580599fd99
Michael Bartling <michael.bartling15+dev@gmail.com>
2014-10-17 12:54:20 -0500

    Added basic framework for NSD Cal service

---
+++ app/src/main/java/com/example/michael/cal/CalNetworkFragment.java
@@ -0,0 +1,63 @@
+package com.example.michael.cal;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ToggleButton;
+
+
+public class CalNetworkFragment extends PlaceholderFragment{
+
+    Activity activity;
+    private static final String ARG_SECTION_NUMBER = "section_number";
+
+    public static CalNetworkFragment newInstance(int sectionNumber) {
+        CalNetworkFragment fragment = new CalNetworkFragment();
+        Bundle args = new Bundle();
+        args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onAttach(Activity activity)
+    {
+        super.onAttach(activity);
+        this.activity = activity;
+    }
+
+    public CalNetworkFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_cal_network, container, false);
+    }
+
+
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        public void onFragmentInteraction(Uri uri);
+    }
+
+}
