a2912c7d6d235bead39d44cd7f7516afb6e0e462
Anthony <ambauer@utexas.edu>
2014-11-06 15:51:29 -0600

    removed image; training activity now saves state

--- app/src/main/java/com/example/michael/cal/TrainingActivity.java
+++ app/src/main/java/com/example/michael/cal/TrainingActivity.java
@@ -13,6 +13,7 @@ import android.opengl.Visibility;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.os.Vibrator;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -29,12 +30,14 @@ public class TrainingActivity extends Activity{
 
     private static CalSqlAdapter calSqlAdapter;
     NthSense sensorService;
+    private CharSequence mTitle;
     Timer T=new Timer();
     TextView text,second;
     Button button;
     DecimalFormat time;
     long startTime,currTime;
     ImageView image;
+    boolean hideImage=true;
     int clickCount = 0;
     Vibrator v;
 
@@ -46,8 +49,15 @@ public class TrainingActivity extends Activity{
     }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    protected void onCreate(Bundle s) {
+        super.onCreate(s);
+        if(s!=null) {
+            clickCount = s.getInt("clickCount");
+            startTime = s.getLong("startTime");
+            currTime = s.getLong("currTime");
+            hideImage = s.getBoolean("hideImage");
+            Log.i("HELPWANTED",""+hideImage);
+        }
         v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
         setContentView(R.layout.activity_training);
         text = (TextView) findViewById(R.id.timeCounter);
@@ -57,35 +67,36 @@ public class TrainingActivity extends Activity{
         Intent intent = new Intent(this, NthSense.class);
         bindService(intent, mConnection, Context.BIND_AUTO_CREATE);
         time = new DecimalFormat(".#");
-
-        text.setText("Press the Start button and place the\n     phone on a flat surface for 10s.");
+        mTitle = getTitle();
+        if(hideImage) {
+            image.setVisibility(View.GONE);
+            button.setVisibility(View.VISIBLE);
+        }
+        else{
+            image.setVisibility(View.VISIBLE);
+            button.setVisibility(View.GONE);
+        }
+        if(clickCount==0)
+            text.setText("Press the Start button and place the\n     phone on a flat surface for 10s.");
+        else if(clickCount==1)
+            image.setImageResource(R.drawable.phoneondesk);
+        else if(clickCount==2) {
+            text.setText("Press the Start button and with the\n        phone in your pocket walk\n                  around for 10s");
+            second.setText("(Phone will vibrate on completion)");
+        }
+        else if(clickCount==3)
+            image.setImageResource(R.drawable.phoneinpocket);
+        else if(clickCount==4) {
+            text.setText("Press the Start button and with the\n        phone in your pocket walk\n                  around for 10s");
+            second.setText("(Phone will vibrate on completion)");
+        }
+        else
+            text.setText("Press the Start button to continue \n               to the main app");
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(clickCount==0){
-                    image.setImageResource(R.drawable.phoneondesk);
-                    sensorService.set_is_takingData(true);
-                }
-                else if(clickCount==2){
-                    image.setImageResource(R.drawable.phoneinpocket);
-                    image.setVisibility(View.VISIBLE);
-                    second.setVisibility(View.GONE);
-                    sensorService.set_is_takingData(true);
-                }
-                else if(clickCount==4){
-                    image.setImageResource(R.drawable.walkingwithphone);
-                    image.setVisibility(View.VISIBLE);
-                    second.setVisibility(View.GONE);
-                    sensorService.set_is_takingData(true);
-                    sensorService.set_is_walking(true);
-                }
-                else if(clickCount==6){
-                    //send app to MainActivity
-                }
-                button.setVisibility(View.GONE);
-                clickCount++;
-                startTime=System.currentTimeMillis();
+                TrainingActivity.this.onClick();
             }
         });
 
@@ -103,6 +114,7 @@ public class TrainingActivity extends Activity{
                             if ((currTime - startTime) > 10000) {
                                 sensorService.set_is_takingData(false);
                                 image.setVisibility(View.GONE);
+                                hideImage=true;
                                 clickCount++;
                                 button.setVisibility(View.VISIBLE);
                                 v.vibrate(500);
@@ -115,11 +127,13 @@ public class TrainingActivity extends Activity{
                             if ((currTime - startTime) > 15000) {
                                 sensorService.set_is_takingData(false);
                                 image.setVisibility(View.GONE);
+                                hideImage=true;
                                 second.setVisibility(View.VISIBLE);
                                 clickCount++;
                                 button.setVisibility(View.VISIBLE);
                                 v.vibrate(500);
                                 text.setText("Press the Start button and with the\n        phone in your pocket walk\n                  around for 10s");
+                                second.setText("(Phone will vibrate on completion)");
                             }
                         }
                         else if(clickCount==5) {
@@ -128,16 +142,55 @@ public class TrainingActivity extends Activity{
                                 sensorService.set_is_takingData(false);
                                 sensorService.set_is_walking(false);
                                 image.setVisibility(View.GONE);
+                                hideImage=true;
                                 clickCount++;
                                 button.setVisibility(View.VISIBLE);
                                 v.vibrate(500);
-                                text.setText("Press the Start button to continue to the main app");
+                                text.setText("Press the Start button to continue \n               to the main app");
                             }
                         }
                     }
                 });
             }
         }, 50, 50);
+    }
+
+    public void onClick(){
+        if(clickCount==0){
+            image.setImageResource(R.drawable.phoneondesk);
+            image.setVisibility(View.VISIBLE);
+            hideImage=false;
+            sensorService.set_is_takingData(true);
+        }
+        else if(clickCount==2){
+            image.setImageResource(R.drawable.phoneinpocket);
+            image.setVisibility(View.VISIBLE);
+            hideImage=false;
+            second.setVisibility(View.GONE);
+            sensorService.set_is_takingData(true);
+        }
+        else if(clickCount==4){
+            image.setVisibility(View.VISIBLE);
+            second.setVisibility(View.GONE);
+            sensorService.set_is_takingData(true);
+            sensorService.set_is_walking(true);
+        }
+        else if(clickCount==6){
+            Intent i = new Intent(TrainingActivity.this, MainActivity.class);
+            startActivity(i);
+        }
+        button.setVisibility(View.GONE);
+        clickCount++;
+        startTime=System.currentTimeMillis();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);                                             //converts boolean to 1 or 0 for storage in Array
+        outState.putInt("clickCount", clickCount);
+        outState.putLong("startTime", startTime);
+        outState.putLong("currTime", currTime);
+        outState.putBoolean("hideImage", hideImage);
     }
 
     @Override
